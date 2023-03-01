67f3745bab7ab696d19c377239ccfa551f5ae087
unknown <waithakameg@gmail.com>
2023-03-01 22:14:29 +0300

    run migrations

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,9 @@
+export TYPEORM_CONNECTION=postgres  
+export TYPEORM_ENTITIES=./src/modules/doamin/**/*.entity.ts
+export TYPEORM_HOST=database-2.c6kpsidrtp6v.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=postres
+export TYPEORM_DATABASE=postgres
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
